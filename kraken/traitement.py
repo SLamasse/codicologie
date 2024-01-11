51ca2bf1ef82a4d79236baf5a92995ad92cf2e9e
SLamasse <74814681+SLamasse@users.noreply.github.com>
2024-01-11 16:18:24 +0100

    Update traitement.py

--- kraken/traitement.py
+++ kraken/traitement.py
@@ -1,2 +1,27 @@
 import kraken
 import kraken.binarization
+# pageseg is the library from pagesegmentation
+from kraken import binarization, pageseg
+from PIL import Image
+from google.colab import files
+
+
+path_image = "../img"
+
+def list_rep(rootdir):
+  list_dir = []
+  for file in os.listdir(rootdir):
+    d = os.path.join(rootdir, file)
+    if os.path.isdir(d):
+        list_dir.append(d)
+  return list_dir
+
+
+
+
+image_filename = '/content/images/page05.jpeg'
+im=Image.open(image_filename)
+bw_im = binarization.nlbin(im)
+seg = pageseg.segment(bw_im)
+
+
