61ecaaab2504df34a120621a05772d7ce9fc1c59
SLamasse <74814681+SLamasse@users.noreply.github.com>
2024-01-14 06:12:21 +0100

    Update main.py

--- kraken/main.py
+++ kraken/main.py
@@ -4,17 +4,6 @@ import alto_to_csv  as a
 import pandas as pd
 
 
-path_image = "../img"
-path_out_alto = "resAlto"
-#On fabrique toutes les reconnaissances de toutes les pages
-k.segmentation_alto(path_image,path_out_alto)
-
-
-#on traite tous les xml alto pour en extraire un tableau par manuscrit qui sera das "resultats"
-path_resultat = "resultats"
-
-
-
 def list_rep(rootdir):
   list_dir = []
   for rep in os.listdir(rootdir) :
@@ -24,14 +13,27 @@ def list_rep(rootdir):
   return list_dir
 
 
-for elt in list_rep(path_out_alto) :
-    data = dict()
-    chemin = path_out_alto + "/" + elt + "/"
-    a.extract_data_manuscript(chemin, data)
-    df = pd.DataFrame(data)
-    outfile = path_resultat + "/" + elt + ".csv"
-    df.to_csv(outfile, index=False)
-    print(df)
+
+
+
+if __name__ == "__main__":
+
+    path_image = "../img"
+    path_out_alto = "resAlto"
+    #On fabrique toutes les reconnaissances de toutes les pages
+    k.segmentation_alto(path_image,path_out_alto)
+
+    #on traite tous les xml alto pour en extraire un tableau par manuscrit qui sera das "resultats"
+    path_resultat = "resultats"
+
+    for elt in list_rep(path_out_alto) :
+        data = dict()
+        chemin = path_out_alto + "/" + elt + "/"
+        a.extract_data_manuscript(chemin, data)
+        df = pd.DataFrame(data)
+        outfile = path_resultat + "/" + elt + ".csv"
+        df.to_csv(outfile, index=False)
+        print(df)
 
 
 
