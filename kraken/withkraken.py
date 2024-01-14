5bd31a7558ead2bcc7d9a3635ea24db46b443e6c
SLamasse <74814681+SLamasse@users.noreply.github.com>
2024-01-14 06:11:37 +0100

    Update withkraken.py

--- kraken/withkraken.py
+++ kraken/withkraken.py
@@ -1,58 +1,47 @@
 import os
 from PIL import Image
-# pageseg is the library from pagesegmentation
-from kraken import binarization, pageseg
-from kraken import blla
+from kraken import blla, lib, pageseg, serialization
 from kraken.lib import vgsl
-from kraken import serialization
 
-
-#model_path = '/usr/local/lib/python3.10/dist-packages/kraken/blla.mlmodel'
-#model_path = 'model/blla_ft_lectau.mlmodel'
-model_path = 'model//blla.mlmodel'
-
+MODEL_PATH = 'model//blla.mlmodel'
 
 
 def list_rep(rootdir):
-  list_dir = []
-  for rep in os.listdir(rootdir):
-    d = os.path.join(rootdir + "/" + rep)
-    if os.path.isdir(d):
-        list_dir.append(rep)
-  return list_dir
+    return [rep for rep in os.listdir(rootdir) if os.path.isdir(os.path.join(rootdir, rep))]
 
 
+def create_output_directory(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
 
-def segmentation_alto(path_image,path_out_alto):
+
+def process_image(image_filename, output_xml):
+    try:
+        im = Image.open(image_filename)
+        seg_model = vgsl.TorchVGSLModel.load_model(MODEL_PATH)
+        baseline_seg = blla.segment(im, model=seg_model)
+        with open(output_xml, 'w') as fp:
+            fp.write(serialization.serialize_segmentation(baseline_seg, image_name=image_filename, image_size=im.size, template='alto'))
+        print(f"Écriture de {output_xml}")
+    except Exception as e:
+        print(f"Erreur lors du traitement de {image_filename}. Erreur : {e}")
+
+
+def segmentation_alto(path_image, path_out_alto):
     for elt in list_rep(path_image):
-        thepath = os.path.join(path_image + "/" + elt)
-        tmp_dir = thepath + "/reframed"
+        thepath = os.path.join(path_image, elt)
+        tmp_dir = os.path.join(thepath, "reframed")
         fichiers = [f for f in os.listdir(tmp_dir) if os.path.isfile(os.path.join(tmp_dir, f))]
-        i = 0
-        for pg in fichiers:
-            image_filename = tmp_dir + "/" + pg
-            try :
-                im=Image.open(image_filename)
-                #bw_im = binarization.nlbin(im)
-                #seg = pageseg.segment(bw_im)
-                seg_model = vgsl.TorchVGSLModel.load_model(model_path)
-                baseline_seg = blla.segment(im, model = seg_model)
-                alto = serialization.serialize_segmentation(baseline_seg, image_name=image_filename, image_size=im.size, template='alto')
 
-                # On test si le reptoire de résultat a été crée pour y déposer les fichier alto ou non
-                dirout = path_out_alto + "/" + elt + "/"
-                if os.path.isdir(dirout):
-                    pass
-                else:
-                    os.mkdir(dirout)
-                filename = os.path.splitext(os.path.basename(pg))
-                output_xml = dirout + "/" + filename[0] + "_alto.xml"
-                with open(output_xml, 'w') as fp:
-                    fp.write(alto)
-                i += 1
-            except:
-                print("La page" + pg + " n'a pas été traité")
-                pass
+        for page_count, pg in enumerate(fichiers, start=1):
+            image_filename = os.path.join(tmp_dir, pg)
+            dirout = os.path.join(path_out_alto, elt)
+            create_output_directory(dirout)
 
-    return
+            filename = os.path.splitext(os.path.basename(pg))
+            output_xml = os.path.join(dirout, f"{filename[0]}_alto.xml")
 
+            if not os.path.exists(output_xml):
+                process_image(image_filename, output_xml)
+            else:
+                print(f"{output_xml} est déjà écrit")
